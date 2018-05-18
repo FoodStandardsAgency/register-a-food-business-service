32cae3053f60a861652304632892107f2ef8f246
Django Shelton <djangoshelton@gmail.com>
2018-05-18 11:43:57 +0100

    add simple email validation

--- src/establishment/establishmentResolvers.js
+++ src/establishment/establishmentResolvers.js
@@ -1,7 +1,12 @@
-const createEstablishment = () => {
+const { isEmail } = require("validator");
+
+const createEstablishment = establishment => {
   // AUTEHNTICATION
 
   // VALIDATION
+  if (!isEmail(establishment.operator_email)) {
+    throw new Error("Invalid email address");
+  }
 
   // RESOLUTION
   return "Establishment Created";
--- src/establishment/establishmentResolvers.test.js
+++ src/establishment/establishmentResolvers.test.js
@@ -1,13 +1,32 @@
 const { createEstablishment } = require("./establishmentResolvers");
 
 describe("Function: createEstablishment", () => {
-  it("Should return Establishment Created", () => {
-    // Arrange
+  describe("When: given invalid email address", () => {
+    it("Should return email error", () => {
+      // Arrange
+      const operator_email = "sdlkf";
+      const error = new Error("Invalid email address");
+      let errorResponse;
+      // Act
+      try {
+        createEstablishment({ operator_email });
+      } catch (err) {
+        // Assert
+        errorResponse = err;
+      }
+      expect(errorResponse).toEqual(error);
+    });
+  });
+  describe("When given valid input", () => {
+    it("Should return Establishment Created", () => {
+      // Arrange
+      const operator_email = "email@email.com";
 
-    // Act
-    const response = createEstablishment();
+      // Act
+      const response = createEstablishment({ operator_email });
 
-    // Assert
-    expect(response).toBe("Establishment Created");
+      // Assert
+      expect(response).toBe("Establishment Created");
+    });
   });
 });
