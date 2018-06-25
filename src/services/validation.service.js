60094ed1a8ba888ba7d5c199aecb3a345ed3c7e9
Django Shelton <djangoshelton@gmail.com>
2018-06-25 12:35:17 +0100

    validation service and tests

---
+++ src/services/validation.service.js
@@ -0,0 +1,38 @@
+const { Validator } = require("jsonschema");
+const schema = require("./validation.schema");
+
+const errorMessages = {
+  declaration1: "Invalid declaration1",
+  declaration2: "Invalid declaration2",
+  declaration3: "Invalid declaration3",
+  operator_first_name: "Invalid operator first name",
+  operator_last_name: "Invalid operator last name",
+  operator_primary_number: "Invalid operator primary number",
+  operator_secondary_number: "Invalid operator secondary number",
+  operator_email: "Invalid operator email",
+  establishment_first_line: "Invalid establishment first line",
+  establishment_street: "Invalid street name",
+  establishment_town: "Invalid town name",
+  establishment_postcode: "Invalid establishment postcode",
+  establishment_trading_name: "Invalid establishment trading name"
+};
+
+const validator = new Validator();
+
+// Set validation rules on validator
+validator.attributes.validation = (instance, schema, options, ctx) => {
+  if (schema.validation(instance) === false) {
+    return errorMessages[ctx.propertyPath.split(".")[1]];
+  }
+};
+
+module.exports.validate = data => {
+  const result = [];
+  const validatorResult = validator.validate(data, schema.establishment);
+  // turn errors into key:value pairs
+  validatorResult.errors.forEach(error => {
+    const key = error.property.split(".")[1];
+    result.push({ key, message: error.message });
+  });
+  return result;
+};
---
+++ src/services/validation.service.test.js
@@ -0,0 +1,59 @@
+jest.mock("./validation.schema", () => ({
+  establishment: {
+    type: "object",
+    properties: {
+      operator_first_name: {
+        type: "string",
+        validation: input => input === "true"
+      },
+      operator_last_name: {
+        type: "string",
+        validation: input => input === "true"
+      },
+      operator_primary_number: {
+        type: "string",
+        validation: input => input === "true"
+      }
+    }
+  }
+}));
+const { validate } = require("./validation.service");
+
+describe("Function: validate", () => {
+  describe("When given valid input", () => {
+    it("Should return empty array", () => {
+      // Arrange
+      const establishment = {
+        operator_first_name: "true",
+        operator_last_name: "true",
+        operator_primary_number: "true"
+      };
+
+      // Act
+      const response = validate(establishment);
+
+      // Assert
+      expect(response).toEqual([]);
+    });
+  });
+
+  describe("When given invalid input", () => {
+    it("Should return array with error for each invalid field", () => {
+      // Arrange
+      const establishment = {
+        operator_first_name: "false",
+        operator_last_name: "false",
+        operator_primary_number: "false"
+      };
+
+      // Act
+      const response = validate(establishment);
+
+      // Assert
+      expect(response.length).toBe(3);
+      expect(response[0].key).toBe("operator_first_name");
+      expect(response[1].key).toBe("operator_last_name");
+      expect(response[2].key).toBe("operator_primary_number");
+    });
+  });
+});
