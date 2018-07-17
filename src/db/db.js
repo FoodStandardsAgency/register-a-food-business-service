ca99a540dcd7c589bfa16ca9d8fb1d1cbd47b4cc
Django Shelton <djangoshelton@gmail.com>
2018-07-17 16:34:06 +0100

    add establishment model

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
+const { info } = require("winston");
+const createEstablishment = require("./models/Establishment");
 
-//postgres://{your_username}:{your_password}@{host_name}:5432/{your_database}?ssl=true
 const connectionString = `postgres://${process.env.POSTGRES_USER}:${
   process.env.POSTGRES_PASS
 }@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DB}?ssl=true`;
@@ -12,12 +13,14 @@ const db = new Sequelize(connectionString, {
   }
 });
 
+const Establishment = createEstablishment(db, Sequelize);
+
 db.authenticate()
   .then(() => {
-    console.log("Connection has been established successfully.");
+    info("Connection to postgres db has been established successfully.");
   })
   .catch(err => {
-    console.error("Unable to connect to the database:", err);
+    info("Unable to connect to the database:", err);
   });
 
-module.exports = db;
+module.exports = { Establishment };
---
+++ src/db/models/Establishment.js
@@ -0,0 +1,35 @@
+module.exports = (db, Sequelize) => {
+  return db.define("establishment", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    operator_type: { type: Sequelize.STRING },
+    operator_first_name: { type: Sequelize.STRING },
+    operator_last_name: { type: Sequelize.STRING },
+    operator_first_line: { type: Sequelize.STRING },
+    operator_street: { type: Sequelize.STRING },
+    operator_town: { type: Sequelize.STRING },
+    operator_postcode: { type: Sequelize.STRING },
+    operator_primary_number: { type: Sequelize.STRING },
+    operator_secondary_number: { type: Sequelize.STRING },
+    operator_email: { type: Sequelize.STRING },
+    operator_company_name: { type: Sequelize.STRING },
+    operator_company_house_number: { type: Sequelize.STRING },
+    operator_charity_name: { type: Sequelize.STRING },
+    operator_charity_number: { type: Sequelize.STRING },
+    establishment_trading_name: { type: Sequelize.STRING },
+    establishment_first_line: { type: Sequelize.STRING },
+    establishment_street: { type: Sequelize.STRING },
+    establishment_town: { type: Sequelize.STRING },
+    establishment_postcode: { type: Sequelize.STRING },
+    establishment_primary_number: { type: Sequelize.STRING },
+    establishment_secondary_number: { type: Sequelize.STRING },
+    establishment_email: { type: Sequelize.STRING },
+    establishment_opening_date: { type: Sequelize.STRING },
+    customer_type: { type: Sequelize.STRING },
+    declaration1: { type: Sequelize.STRING },
+    declaration2: { type: Sequelize.STRING },
+    declaration3: { type: Sequelize.STRING }
+  });
+};
