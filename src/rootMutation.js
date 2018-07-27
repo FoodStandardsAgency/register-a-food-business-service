6def2851262b662063a59f08b343963ccea9e10f
Django Shelton <djangoshelton@gmail.com>
2018-07-27 09:22:22 +0100

    remove uneeded graphql files

--- src/rootMutation.js
+++
@@ -1,9 +0,0 @@
-const { GraphQLObjectType } = require("graphql");
-const createEstablishment = require("./establishment/establishmentMutation");
-
-module.exports = new GraphQLObjectType({
-  name: "EstablishmentMutation",
-  fields: () => ({
-    createEstablishment
-  })
-});
