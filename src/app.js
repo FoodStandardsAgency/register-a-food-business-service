ee427f47473a5c2e2dca9cb056d19d47ce596dea
Django Shelton <djangoshelton@gmail.com>
2018-08-17 11:30:56 +0100

    add helmet

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const winston = require("winston");
 const bodyParser = require("body-parser");
+const helmet = require("helmet")
 
 const { routers } = require("./api/routers");
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use("/", routers());
 
