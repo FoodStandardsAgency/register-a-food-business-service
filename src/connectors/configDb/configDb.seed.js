8097b34d0701dcd29fda9d2800d6220de8832816
Joe Brady <joe.brady@uk.ibm.com>
2018-08-28 14:39:47 +0100

    Update config db seed

--- src/connectors/configDb/configDb.seed.js
+++ src/connectors/configDb/configDb.seed.js
@@ -2,53 +2,69 @@ const mongoClient = require("mongodb").MongoClient;
 const { CONFIGDB_URL } = require("../../config");
 const { info } = require("winston");
 
+// _id fields are from https://data.food.gov.uk/codes/reference-number/_authority
 const arrayToInsert = [
-  {
-    // IDs are from https://data.food.gov.uk/codes/reference-number/_authority
-    _id: 6008,
-    local_council: "Mid & East Antrim Borough Council",
-    local_council_email: "food@midandeastantrim.gov.uk",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
-    local_council_url: "mid-and-east-antrim"
-  },
   {
     _id: 8020,
     local_council: "Bridgend County Borough Council",
-    local_council_email: "fsatestemail.valid@gmail.com",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+    local_council_email: "publicprotection@bridgend.gov.uk",
+    local_council_notify_emails: ["publicprotection@bridgend.gov.uk"],
     local_council_url: "bridgend"
   },
   {
-    _id: 4221,
-    local_council: "West Dorset District Council",
-    local_council_email: "fsatestemail.valid@gmail.com",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
-    local_council_url: "west-dorset",
-    separate_standards_council: 4226
+    _id: 8015,
+    local_council: "City of Cardiff Council",
+    local_council_email: "foodteam@cardiff.gov.uk",
+    local_council_notify_emails: ["foodteam@cardiff.gov.uk"],
+    local_council_url: "cardiff"
   },
   {
-    _id: 4223,
-    local_council: "North Dorset District Council",
-    local_council_email: "fsatestemail.valid@gmail.com",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
-    local_council_url: "north-dorset",
-    separate_standards_council: 4226
-  },
-  {
-    _id: 4220,
-    local_council: "Weymouth and Portland Borough Council",
-    local_council_email: "fsatestemail.valid@gmail.com",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
-    local_council_url: "weymouth-and-portland",
-    separate_standards_council: 4226
-  },
-  {
-    _id: 4226,
-    local_council: "Dorset County Council",
-    local_council_email: "fsatestemail.valid@gmail.com",
-    local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
-    local_council_url: ""
+    _id: 8002,
+    local_council: "Vale of Glamorgan Council",
+    local_council_email: "AdminSupport-SRSVale@valeofglamorgan.gov.uk",
+    local_council_notify_emails: [
+      "AdminSupport-SRSVale@valeofglamorgan.gov.uk"
+    ],
+    local_council_url: "the-vale-of-glamorgan"
   }
+  // {
+  //   _id: 6008,
+  //   local_council: "Mid & East Antrim Borough Council",
+  //   local_council_email: "food@midandeastantrim.gov.uk",
+  //   local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+  //   local_council_url: "mid-and-east-antrim"
+  // },
+  // {
+  //   _id: 4221,
+  //   local_council: "West Dorset District Council",
+  //   local_council_email: "fsatestemail.valid@gmail.com",
+  //   local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+  //   local_council_url: "west-dorset",
+  //   separate_standards_council: 4226
+  // },
+  // {
+  //   _id: 4223,
+  //   local_council: "North Dorset District Council",
+  //   local_council_email: "fsatestemail.valid@gmail.com",
+  //   local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+  //   local_council_url: "north-dorset",
+  //   separate_standards_council: 4226
+  // },
+  // {
+  //   _id: 4220,
+  //   local_council: "Weymouth and Portland Borough Council",
+  //   local_council_email: "fsatestemail.valid@gmail.com",
+  //   local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+  //   local_council_url: "weymouth-and-portland",
+  //   separate_standards_council: 4226
+  // },
+  // {
+  //   _id: 4226,
+  //   local_council: "Dorset County Council",
+  //   local_council_email: "fsatestemail.valid@gmail.com",
+  //   local_council_notify_emails: ["fsatestemail.valid@gmail.com"],
+  //   local_council_url: ""
+  // }
 ];
 
 let client;
