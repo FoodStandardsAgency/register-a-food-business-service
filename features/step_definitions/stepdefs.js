29b0c2db4a7fe30cd200b889335ec3f488bdab6e
libertyjacklin1 <liberty.jacklin@gmail.com>
2018-09-09 15:21:58 +0100

    Tidying - getting rid of unused stepdefs

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -35,89 +35,7 @@ const getRequest = async id => {
   );
   return res.json();
 };
-Given("I enter an email address without an @ symbol", function() {
-  this.emailAddress = "skdjfh";
-});
-
-When("I submit the email to the back end application", async function() {
-  const requestBody = JSON.stringify({
-    query: `mutation { createEstablishment(id: 1, operator_email: "${
-      this.emailAddress
-    }") {id} }`
-  });
-  this.response = await sendRequest(requestBody);
-});
-
-Then("I get an email error response", function() {
-  assert.equal(this.response.errors[0].message, "The request is invalid.");
-  assert.equal(this.response.errors[0].state.email, "Invalid email address");
-});
-
-Then("I get an invalid phone number response", function() {
-  assert.equal(this.response.errors[0].message, "The request is invalid.");
-  this.response.errors[0].state.operator_mobile_numbers.forEach(err => {
-    assert.equal(err, "Invalid phone number");
-  });
-});
-
-Then("I get multiple invalid phone number responses", function() {
-  this.response.errors[0].state.operator_mobile_numbers.forEach(err => {
-    assert.equal(err, "Invalid phone number");
-  });
-});
-
-Given("I enter an email address with two @ symbols", function() {
-  this.emailAddress = "testing@twosymbols@test";
-});
-
-Given("I enter a mobile number longer than 11 digits long", function() {
-  this.mobileNumber = ["01234567890123"];
-});
-
-Given("I enter multiple phone numbers longer than 11 digits long", function() {
-  this.mobileNumber = ["012345678938764", "01233129378329934243"];
-});
-
-When(
-  "I submit the mobile number to the back end application",
-  async function() {
-    const requestBody = JSON.stringify({
-      query: `mutation { createEstablishment(id: 1, operator_mobile_numbers: "${
-        this.mobileNumber
-      }") {id} }`
-    });
-    this.response = await sendRequest(requestBody);
-  }
-);
-
-Then("I get an error response for mobile", function() {
-  assert.equal(this.response.errors[0].message, "invalid mobile sent");
-});
-
-Given(
-  "I enter a mobile number exactly 11 characters long including spaces, but with less than 11 digits",
-  function() {
-    this.mobileNumber = ["012345 6789"];
-  }
-);
-
-When(
-  "I submit the mobile number that includes a space to the back end application",
-  async function() {
-    const requestBody = JSON.stringify({
-      query: `mutation { createEstablishment(id: 1, operator_mobile_numbers: "${
-        this.mobileNumber
-      }") {id} }`
-    });
-    this.response = await sendRequest(requestBody);
-  }
-);
-
-Then("I get an error response for mobile due to the space", function() {
-  assert.equal(this.response.errors[0].message, "invalid mobile sent");
-});
-
-//////////////
+////////
 
 Given("I have a new registration with all valid required fields", function() {
   this.registration = {
@@ -255,6 +173,7 @@ Then("I get an error response", function() {
 Then("The non personal information is saved to the database", async function() {
   const id = this.response.regId;
   this.response = await getRequest(id);
+  console.log(this.response);
   assert.equal(this.response.establishment.establishment_trading_name, "Itsu");
 });
 
