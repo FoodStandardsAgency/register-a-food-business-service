f3770d0eb1b18d24877dd75d75a9beab77ca42c8
A. P. Malde <anisha.malde@ibm.com>
2018-10-04 13:04:35 +0100

    database fields

--- src/db/migrations/v1.2.0-add-opening-days.js
+++ src/db/migrations/v1.2.0-add-opening-days.js
@@ -3,8 +3,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
       "activities",
-      "business_opening_days",
-      Sequelize.STRING
+      "business_irregular_days",
+      Sequelize.STRING,
+      "opening_days_monday",
+      Sequelize.BOOLEAN,
+      "opening_days_tuesday",
+      Sequelize.BOOLEAN,
+      "opening_days_wednesday",
+      Sequelize.BOOLEAN,
+      "opening_days_thursday",
+      Sequelize.BOOLEAN,
+      "opening_days_friday",
+      Sequelize.BOOLEAN,
+      "opening_days_saturday",
+      Sequelize.BOOLEAN,
+      "opening_days_sunday",
+      Sequelize.BOOLEAN
     );
   },
   down: queryInterface => {
--- src/db/models/Activities.js
+++ src/db/models/Activities.js
@@ -10,7 +10,16 @@ module.exports = (sequelize, DataTypes) => {
       customer_type: { type: DataTypes.STRING },
       business_type: { type: DataTypes.STRING },
       business_type_search_term: { type: DataTypes.STRING },
-      import_export_activities: { type: DataTypes.STRING }
+      import_export_activities: { type: DataTypes.STRING },
+      business_other_details: { type: DataTypes.STRING },
+      business_irregular_days: { type: DataTypes.STRING },
+      opening_days_monday: { type: DataTypes.BOOLEAN },
+      opening_days_tuesday: { type: DataTypes.BOOLEAN },
+      opening_days_wednesday: { type: DataTypes.BOOLEAN },
+      opening_days_thursday: { type: DataTypes.BOOLEAN },
+      opening_days_friday: { type: DataTypes.BOOLEAN },
+      opening_days_saturday: { type: DataTypes.BOOLEAN },
+      opening_days_sunday: { type: DataTypes.BOOLEAN }
     },
     {}
   );
