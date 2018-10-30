1924624bb12de066107fd5c6fc0cfd39312806a1
A. P. Malde <anisha.malde@ibm.com>
2018-10-30 14:40:58 +0000

    Changes to config

---
+++ src/services/pdf-config.js
@@ -0,0 +1,149 @@
+const path = require("path");
+
+const docDefinition = {
+  content: [
+    {
+      canvas: [
+        {
+          type: "rect",
+          x: 1,
+          y: 1,
+          w: 505,
+          h: 185,
+          r: 4,
+          color: "#28a197",
+          alignment: "center"
+        }
+      ]
+    },
+    {
+      text: [
+        { text: "Your unique food business ", color: "white", fontSize: 36 },
+        { text: "registration number is", color: "white", fontSize: 36 }
+      ],
+      absolutePosition: { x: 30, y: 60 },
+      alignment: "center"
+    },
+    {
+      text: "F3KEQE - G6JESF - QKMNFN",
+      absolutePosition: { x: 30, y: 150 },
+      color: "white",
+      fontSize: 36,
+      bold: true,
+      alignment: "center"
+    },
+    "\n\n\n\n",
+    {
+      style: "bigger",
+      text: "Operator details",
+      bold: true
+    },
+    {
+      canvas: [
+        {
+          type: "rect",
+          x: 1,
+          y: 1,
+          w: 505,
+          h: 1,
+          color: "#808080"
+        }
+      ]
+    },
+    "\n",
+    {
+      style: "bigger",
+      columns: [
+        {
+          width: "*",
+          text: "Operator type"
+        },
+        {
+          width: "*",
+          style: "header",
+          text: "Sole trader"
+        }
+      ]
+    },
+    "\n",
+    {
+      canvas: [
+        {
+          type: "rect",
+          x: 1,
+          y: 1,
+          w: 505,
+          h: 1,
+          color: "#808080"
+        }
+      ]
+    },
+    "\n",
+    {
+      style: "bigger",
+      columns: [
+        {
+          width: "*",
+          text: "Operator address"
+        },
+        {
+          width: "*",
+          style: "header",
+          text: "Manor Farm Barns \n Fox Road \n Norwich \n NR14 7PZ"
+        }
+      ]
+    },
+    "\n",
+    {
+      canvas: [
+        {
+          type: "rect",
+          x: 1,
+          y: 1,
+          w: 505,
+          h: 1,
+          color: "#808080"
+        }
+      ]
+    }
+  ],
+  styles: {
+    header: {
+      bold: true
+    },
+    bigger: {
+      fontSize: 19
+    }
+  },
+
+  defaultStyle: {
+    columnGap: 20,
+    fontSize: 6
+  }
+};
+
+const fontDescriptors = {
+  Roboto: {
+    normal: path.join(
+      __dirname,
+      "..",
+      "/services",
+      "/fonts/Roboto-Regular.ttf"
+    ),
+    bold: path.join(__dirname, "..", "/services", "/fonts/Roboto-Medium.ttf"),
+    italics: path.join(
+      __dirname,
+      "..",
+      "/services",
+      "/fonts/Roboto-Italic.ttf"
+    ),
+    bolditalics: path.join(
+      __dirname,
+      "..",
+      "/services",
+      "/fonts/Roboto-MediumItalic.ttf"
+    )
+  }
+};
+
+module.exports = { docDefinition, fontDescriptors };
--- src/services/pdf.services.test.js
+++ src/services/pdf.services.test.js
@@ -1,7 +1,11 @@
+const { docDefinition, fontDescriptors } = require("./pdf-config");
 const { pdfGenerator } = require("./pdf.service");
 
 describe("Pdf Generator function", () => {
   let result;
+  const clonedDocDef = JSON.parse(JSON.stringify(docDefinition));
+  const clonedFontDes = JSON.parse(JSON.stringify(fontDescriptors));
+
   beforeEach(async () => {
     result = await pdfGenerator();
   });
@@ -11,5 +15,11 @@ describe("Pdf Generator function", () => {
     expect(typeof result).toBe("string");
   });
 
-  it("doesnt modify the original doc definition", () => {});
+  it("doesnt modify the original doc definition", () => {
+    expect(docDefinition).toEqual(clonedDocDef);
+  });
+
+  it("doesnt modify the original font descriptor", () => {
+    expect(fontDescriptors).toEqual(clonedFontDes);
+  });
 });
