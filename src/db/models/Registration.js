38431256ea78baa4bf84887ddfa043f7cdf611b3
A. P. Malde <anisha.malde@ibm.com>
2018-11-13 14:49:46 +0000

    Add to model

--- src/db/models/Registration.js
+++ src/db/models/Registration.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
       },
       council: {
         type: DataTypes.STRING
+      },
+      collected: {
+        type: DataTypes.BOOLEAN
       }
     },
     {}
