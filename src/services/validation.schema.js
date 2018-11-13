1aebdeec2c6c335edddef6464969d53ed27fe04d
A. P. Malde <anisha.malde@ibm.com>
2018-11-13 14:52:49 +0000

    Update to schema

--- src/services/validation.schema.js
+++ src/services/validation.schema.js
@@ -269,11 +269,7 @@ const schema = {
         properties: {
           declaration1: { type: "string", validation: validateDeclaration },
           declaration2: { type: "string", validation: validateDeclaration },
-          declaration3: { type: "string", validation: validateDeclaration },
-          collected: {
-            type: "boolean",
-            validation: validateOpeningDay
-          }
+          declaration3: { type: "string", validation: validateDeclaration }
         },
         required: ["declaration1", "declaration2", "declaration3"]
       }
