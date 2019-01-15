c1d192e97997ade74e5b3d81d268aef022d68cda
Django Shelton <djangoshelton@gmail.com>
2019-01-15 12:59:08 +0000

    fix business_other_details not being long enough

---
+++ src/db/migrations/v1.7.0-update-business-other-details-length.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn("activities", "business_other_details", {
+      type: Sequelize.STRING(1500)
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn("activities", "business_other_details", {
+      type: Sequelize.STRING
+    });
+  }
+};
