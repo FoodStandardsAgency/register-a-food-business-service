72b0f6a12a9aecbff87323b1b810e63f0c5a175f
Michal Rak <michal.rak@civica.co.uk>
2019-07-12 09:50:02 +0000

    add db migration for water supply

---
+++ src/db/migrations/v1.9.1-add-water-supply.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn("activities", "water_supply", Sequelize.STRING)
+    ]);
+  },
+  down: queryInterface => {
+    return Promise.all([
+      queryInterface.removeColumn("activities", "water_supply")
+    ]);
+  }
+};
