dd22f6283ce589efd66782fef024f6c3250df0e6
lukevincentcivica <lvincent@cwhite-fsa-vm.v4d1dq0mhvke5n2u4f5khetsbh.cwx.internal.cloudapp.net>
2019-10-15 10:02:58 +0000

    Add feedback column to Metadata table in database

---
+++ src/db/migrations/v2.3.0-add-feedback.js
@@ -0,0 +1,11 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn("metadata", "feedback1", Sequelize.STRING)
+    ]);
+  },
+  down: queryInterface => {
+    return Promise.all([queryInterface.removeColumn("metadata", "feedback1")]);
+  }
+};
