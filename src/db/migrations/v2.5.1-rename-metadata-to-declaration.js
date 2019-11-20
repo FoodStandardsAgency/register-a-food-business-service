7964b5a8c15b5e2f823a0c4de6405a3ff940bec7
lukevincentcivica <lvincent@cwhite-fsa-vm.v4d1dq0mhvke5n2u4f5khetsbh.cwx.internal.cloudapp.net>
2019-11-20 10:38:12 +0000

    migration script to rename metadata to declaration

---
+++ src/db/migrations/v2.5.1-rename-metadata-to-declaration.js
@@ -0,0 +1,13 @@
+"use strict";
+module.exports = {
+  up: queryInterface => {
+    return Promise.all([
+      queryInterface.renameTable("metadata", "declarations")
+    ]);
+  },
+  down: queryInterface => {
+    return Promise.all([
+      queryInterface.renameTable("declarations", "metadata")
+    ]);
+  }
+};
