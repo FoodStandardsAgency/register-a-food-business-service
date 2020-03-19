ac8e0cb9bc21a03dc1ba214526ef063058fdf26f
Adam White <Adam.White@Civica.co.uk>
2020-03-19 11:51:07 +0000

    Add test config for db.

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -21,6 +21,13 @@ module.exports = {
       ssl: true
     }
   },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres"
+  },
   local: {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASS,
