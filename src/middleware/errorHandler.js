5cfa0dc81bfc5bcc5275902f9dc4e1b1fbb3b453
Simon Thomas <simon.thomas@civica.co.uk>
2020-03-30 15:45:49 +0000

    fix format

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -18,10 +18,7 @@ const { logEmitter, ERROR } = require("../../services/logging.service");
 const errorDetails = require("./errors.json");
 
 const errorHandler = (err, req, res, next) => {
-  logEmitter.emit(
-    ERROR,
-    `Application error: ${JSON.stringify(err)}`
-  );
+  logEmitter.emit(ERROR, `Application error: ${JSON.stringify(err)}`);
   if (err.name) {
     const errorDetail = errorDetails.find(error => {
       return error.name === err.name;
