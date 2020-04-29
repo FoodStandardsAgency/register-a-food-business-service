3cb41457c0396b8e6e910fba546baf941da27d4b
Adam White <awhite234@gmail.com>
2020-04-29 16:10:52 +0100

    Add docker_init proxy lines (local dev only)

--- docker_init.sh
+++ docker_init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+npm config set -g proxy "${DOCKER_HTTP_PROXY}" && \
+npm config set -g https-proxy "${DOCKER_HTTPS_PROXY}" && \
 npm config set -g progress=false && \
 npm config set -g strict-ssl false && \
 npm config set -g cache /opt/npmcache && \
