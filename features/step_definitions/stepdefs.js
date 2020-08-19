bfaf739481b322d892e23dc9959f8bfe77188230
Paul Burnett <paul.burnett@civica.co.uk>
2020-08-19 11:53:15 +0000

    Revert default API URL

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -10,7 +10,7 @@ const {
 
 setDefaultTimeout(60 * 1000);
 
-let apiUrl = "http://localhost:4000";
+let apiUrl = "https://test-register-a-food-business-service.azurewebsites.net";
 if(process.env.NODE_ENV === 'local'){
   apiUrl = process.env.API_URL ? process.env.API_URL : apiUrl;
 }
