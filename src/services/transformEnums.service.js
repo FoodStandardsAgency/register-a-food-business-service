1c6c645a8850a00769225411d360a28e04807279
Paul Burnett <paul.burnett@civica.co.uk>
2021-02-02 10:03:36 +0000

    Improve enum transforms and additional tests

--- src/services/transformEnums.service.js
+++ src/services/transformEnums.service.js
@@ -9,47 +9,41 @@ const {
 
 // Functions that convert enums from keys to values.
 
-const transformEstablishmentTypeEnum = (establishmentType, language) => {
-  const lang = language || "en";
+const transformEstablishmentTypeEnum = (establishmentType, language = "en") => {
   return establishmentTypeEnum[establishmentType]
-    ? establishmentTypeEnum[establishmentType].value[lang]
+    ? establishmentTypeEnum[establishmentType].value[language]
     : null;
 };
 
-const transformOperatorTypeEnum = (operatorType, language) => {
-  const lang = language || "en";
+const transformOperatorTypeEnum = (operatorType, language = "en") => {
   return operatorTypeEnum[operatorType]
-    ? operatorTypeEnum[operatorType].value[lang]
+    ? operatorTypeEnum[operatorType].value[language]
     : null;
 };
 
-const transformWaterSupplyEnum = (waterSupply, language) => {
-  const lang = language || "en";
+const transformWaterSupplyEnum = (waterSupply, language = "en") => {
   return waterSupplyEnum[waterSupply]
-    ? waterSupplyEnum[waterSupply].value[lang]
+    ? waterSupplyEnum[waterSupply].value[language]
     : null;
 };
 
 const transformBusinessImportExportEnum = (
   importExportActivities,
-  language
+  language = "en"
 ) => {
-  const lang = language || "en";
   return importExportEnum[importExportActivities]
-    ? importExportEnum[importExportActivities].value[lang]
+    ? importExportEnum[importExportActivities].value[language]
     : null;
 };
 
-const transformCustomerTypeEnum = (customerType, language) => {
-  const lang = language || "en";
+const transformCustomerTypeEnum = (customerType, language = "en") => {
   return customerTypeEnum[customerType]
-    ? customerTypeEnum[customerType].value[lang]
+    ? customerTypeEnum[customerType].value[language]
     : null;
 };
 
-const transformBusinessTypeEnum = (id, language) => {
-  const lang = language || "en";
-  return businessTypeEnum[id] ? businessTypeEnum[id].value[lang] : null;
+const transformBusinessTypeEnum = (id, language = "en") => {
+  return businessTypeEnum[id] ? businessTypeEnum[id].value[language] : null;
 };
 
 module.exports = {
