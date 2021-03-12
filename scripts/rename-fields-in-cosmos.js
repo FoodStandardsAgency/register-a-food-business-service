a0ca7e87f4f9343e7bf16a077b7a6f16e8f43ba2
Joshua Perry <joshua.perry@civica.co.uk>
2021-03-12 12:03:57 +0000

    Add script to reanem old address fields

--- scripts/rename-fields-in-cosmos.js
+++ scripts/rename-fields-in-cosmos.js
@@ -112,11 +112,77 @@ const renameDirectSubmission = async () => {
   }
 };
 
+const renameAddressFields = async () => {
+  try {
+    beCache = await establishConnectionToCosmos(
+      "registrations",
+      "registrations"
+    );
+    addressRecords = await findRecords({
+      $or: [
+        { "establishment.premise.establishment_first_line": { $exists: true } },
+        { "establishment.operator.operator_first_line": { $exists: true } }
+      ]
+    });
+    logEmitter.emit(
+      "info",
+      `${addressRecords.length} records containing old address fields`
+    );
+
+    while (addressRecords.length > 0) {
+      const promises = addressRecords.slice(0, 50).map(async (rec) => {
+        addressRecords = addressRecords.filter((reg) => {
+          return reg !== rec;
+        });
+        await beCache.updateOne(
+          { "fsa-rn": rec["fsa-rn"] },
+          {
+            $rename: {
+              "establishment.operator.operator_first_line":
+                "operator_address_line_1",
+              "establishment.operator.operator_street":
+                "operator_address_line_2",
+              "establishment.operator.operator_dependent_locality":
+                "operator_address_line_3",
+              "establishment.premise.establishment_first_line":
+                "establishment_address_line_1",
+              "establishment.premise.establishment_street":
+                "establishment_address_line_2",
+              "establishment.premise.establishment_dependent_locality":
+                "establishment_address_line_3"
+            }
+          }
+        );
+      });
+      await Promise.allSettled(promises);
+    }
+
+    const remainingAddressRecords = await findRecords({
+      $or: [
+        { "establishment.premise.establishment_first_line": { $exists: true } },
+        { "establishment.operator.operator_first_line": { $exists: true } }
+      ]
+    });
+    const remainingFsaRns = getFsaRns(remainingAddressRecords);
+    logEmitter.emit(
+      "info",
+      `${remainingFsaRns.length} records still containing old address fields - ${remainingFsaRns}`
+    );
+  } catch (err) {
+    logEmitter.emit(
+      "info",
+      `Failed to rename records with old address fields: ${err}`
+    );
+  }
+};
+
 renameToDeclaration()
   .then(() => {
     renameDirectSubmission().then(() => {
-      closeCosmosConnection();
-      logEmitter.emit("info", "Successfully finished rename script");
+      renameAddressFields().then(() => {
+        closeCosmosConnection();
+        logEmitter.emit("info", "Successfully finished rename script");
+      });
     });
   })
   .catch((err) => {
